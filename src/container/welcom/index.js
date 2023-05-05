9487cd6a656dc66eda4f2eb85346eff9f5a6ae24
Khaldoon Khaldi <khaldoonpc@Khaldoons-MacBook-Pro.local>
2023-05-05 16:13:36 +0300

    fetch one user

--- src/container/welcom/index.js
+++ src/container/welcom/index.js
@@ -1,27 +1,36 @@
 import React from 'react';
 // import {app} from '../../firebaseConfig';
 import { db } from '../../firebaseConfig';
-import { collection, getDocs, where, query } from "firebase/firestore"; 
+import { collection, getDocs, where, query, getDoc, doc } from "firebase/firestore"; 
 
 function WelcomePage() {
-    // const authToken = localStorage.getItem('authToken');
+    const authToken = localStorage.getItem('authToken');
     // const db = app.firestore();
     const getUserById = async (userId) => {
-      const querySnapshot = await getDocs(collection(db, "users"));
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-      });
+      const docRef = doc(db, "users", userId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+      } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document!");
+      }
+      // const querySnapshot = await getDocs(collection(db, "users"));
+      // querySnapshot.forEach((doc) => {
+      //   // doc.data() is never undefined for query doc snapshots
+      //   console.log(doc.id, " => ", doc.data());
+      // });
     }
     getUserById("djFRPDu0OhPwda3yMOdD")
 
-    // if (authToken) {
-    //     console.log(authToken)
-    //     getUserById("djFRPDu0OhPwda3yMOdD")
-    //     // Authentication token exists in localStorage, do something with it
-    //   } else {
-    //     // Authentication token does not exist in localStorage
-    //   }
+    if (authToken) {
+        // console.log(authToken)
+        getUserById("djFRPDu0OhPwda3yMOdD")
+        // Authentication token exists in localStorage, do something with it
+      } else {
+        
+        // Authentication token does not exist in localStorage
+      }
 
 
   return (
