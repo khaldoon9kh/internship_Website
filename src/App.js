69787323039c9ac4e842c1e614e0bb7c638348d3
Khaldoon Khaldi <khaldoonpc@Khaldoons-MacBook-Pro.local>
2023-05-05 16:36:33 +0300

    add sidebar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import './App.css';
 import HeaderLogin from "../src/component/headerLogin";
 import LoginPage from "../src/container/login";
 import WelcomePage from "../src/container/welcom";
+import SideBar from '../src/component/navSidebar'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
+    // <Router>
+    //   <HeaderLogin/>
+    //   <Routes>
+    //     <Route path="/" element={<LoginPage />} />
+    //     <Route path="/welcome" element={<WelcomePage />} />
+    //   </Routes>
+    // </Router>
     <Router>
-      <HeaderLogin/>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/welcome" element={<WelcomePage />} />
-      </Routes>
+      <div className="wrapper">
+        <div className="sidebar">
+          <SideBar/>
+        </div>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/welcome" element={<WelcomePage />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
       
   );
---
+++ src/component/navSidebar/index.jsx
@@ -0,0 +1,12 @@
+import uniLogo from '../../img/uni_logo.png'; // import your logo image file
+import "./index.css";
+
+function SideBar() {
+  return (
+    <div className="sideMainCont">
+     
+    </div>
+  );
+}
+
+export default SideBar;
