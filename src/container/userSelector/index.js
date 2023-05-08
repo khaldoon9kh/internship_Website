4c40d296be9f8c748be7db3b65996899ff923171
Khaldoon Khaldi <khaldoonpc@Khaldoons-MacBook-Pro.local>
2023-05-08 16:12:12 +0300

    build page

---
+++ src/container/userSelector/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./index.css";
+
+function UserSelector() {
+  
+  return (
+    <div className="user-selector">
+      <h1>Select User Type</h1>
+      <div className="button-container">
+        <Link to="/login?type=admin" className="button admin-button">
+          Admin
+        </Link>
+        <Link to="/login?type=student" className="button student-button">
+          Student
+        </Link>
+        <Link to="/login?type=internship-coordinator" className="button coordinator-button">
+          Internship Coordinator
+        </Link>
+        <Link to="/login?type=career-center" className="button center-button">
+          Career Center
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default UserSelector;
