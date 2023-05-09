1c1e0e0d27746ae0bbf96549a879a59e88060634
AmeerAG <ameerag@AmeerAGs-MacBook-Pro.local>
2023-05-09 15:14:16 +0300

    creat page

---
+++ src/container/coordinatorDashboard/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import "./index.css";
+
+
+function CoordinatorDashboard() {
+  return(
+    <div>
+      <h2>
+        hi
+      </h2>
+    </div>
+  )
+}
+
+export default CoordinatorDashboard;
