a56b226be1a4d20b31ed88a14858babdfbd77c28
Khaldoon Khaldi <khaldoonpc@Khaldoons-MacBook-Pro.local>
2023-05-10 13:24:39 +0300

    fix logos

--- src/component/navSidebar/index.jsx
+++ src/component/navSidebar/index.jsx
@@ -1,54 +1,42 @@
 import { ReactComponent as NotificationLogo } from "../../svgs/notification-status.svg";
 import { ReactComponent as MailLogo } from "../../svgs/message-notif.svg";
 import { ReactComponent as NotLogo } from "../../svgs/notification.svg";
-import { ReactComponent as profileLogo } from "../../svgs/profile.svg";
-import { ReactComponent as homepageLogo } from "../../svgs/homepage.svg";
-import { ReactComponent as logoutLogo } from "../../svgs/log out.svg";
-import myImage from '../../img/logo.png';
+import { ReactComponent as ProfileLogo } from "../../svgs/profile.svg";
+import { ReactComponent as JobCenter } from "../../svgs/briefcase-1944.svg";
+import { ReactComponent as LogoutLogo } from "../../svgs/logOut.svg";
+import uniLogo from '../../img/uni_logo.png';
 import "./index.css";
 
 function SideBar() {
   return (
     <div className="sideMainCont">
       <div className="logo">
-      <img src={myImage} alt="My Image" className="my-image" />
-
-
+        <img src={uniLogo} alt="My Image" className="my-image" />
       </div>
-     <div className="navbar-container">
-    <svg className="navbar-icon" />
-    <homepageLogo/>
-    <a href="/" className="navbar-link">Homepage</a>
-    </div>
-     <div className="navbar-container">
-    <svg className="navbar-icon" />
-    <homepageLogo/>
+
+    <div className="navbar-container">
+    <ProfileLogo/>
     <a href="/" className="navbar-link">Profile</a>
     </div>
      <div className="navbar-container">
-    <svg className="navbar-icon" />
     <NotificationLogo/>
     <a href="/" className="navbar-link">Application status</a>
     </div>
      <div className="navbar-container">
-    <svg className="navbar-icon" />
     <MailLogo/>
     <a href="/" className="navbar-link">Mail</a>
     </div>
      <div className="navbar-container">
-    <svg className="navbar-icon" />
-    
+    <JobCenter/>
     <a href="/" className="navbar-link">Career center</a>
     </div>
      <div className="navbar-container">
-    <svg className="navbar-icon" />
     <NotLogo/>
     <a href="/" className="navbar-link">Notification</a>
     </div>
     <div className="specail">
      <div className="navbar-container">
-    <svg className="navbar-icon" />
-    <logoutLogo/>
+    <LogoutLogo/>
     <a href="/" className="navbar-link">Log out</a>
     </div>
     </div>
