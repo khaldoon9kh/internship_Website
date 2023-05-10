461723177ab2f746d7b1f0c65382741ea790c457
AmeerAG <ameerag@AmeerAGs-MacBook-Pro.local>
2023-05-10 21:13:01 +0300

    page is ready Without function, and icons

--- src/container/coordinatorDashboard/index.jsx
+++ src/container/coordinatorDashboard/index.jsx
@@ -4,10 +4,49 @@ import "./index.css";
 
 function CoordinatorDashboard() {
   return(
-    <div>
-      <h2>
-        hi
-      </h2>
+    <div className='coordinator-dashboeard-outerContainer'>
+     <div className='coordinator-dashboeard-mainContainer'>
+        <h2>Internship applications coming from internship coordinator</h2>
+
+        <div  className='coordinator-dashboeard-internshipCells'>
+          <div class="flex-container1">
+            <div className='internshipInfo'>
+              <h4 className='internship-name'>Software Internship</h4>
+              <h6 className='internship-dateSent'>date sent 
+                <time className='dateTime' datetime="DD-MM-YY"> 4/3/23</time>
+              </h6>
+            </div>
+            <div className='file-download-icon'>
+              <button>D</button>
+            </div>
+            <div class = "vertical-line" className='vertical-line'>
+              {/* VErtival Line */}
+            </div>
+            <div className='intern-info'>
+              <h4>Internship  application coming from  :  </h4> 
+              <h3 className='intern-name'>Ameer AbuGharbiah</h3>
+            </div>
+            <div className='sgk-button-container'>
+              <button className='sgk-button'>Issue SGK </button>
+            </div>
+          </div>
+          
+        </div>
+        <div className='coordinator-dashboeard-internshipCells'>
+          {/* <div class="flex-container">
+            <div>1
+              <h4 className='internship-name'>Software Internship</h4>
+              <h6 className='internship-dateSent'>date sent</h6>
+            </div>
+            <div>2</div>
+            <div>3</div>
+          </div> */}
+        </div>
+      
+        <div className='coordinator-dashboeard-internshipCells'></div>
+        <div className='coordinator-dashboeard-internshipCells'></div>
+     
+     </div>
     </div>
   )
 }
