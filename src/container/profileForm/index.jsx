34bd805ff2c43010e85c2f293e6a4bc42df3f522
KhaldoonKH <khaldoon9kh9@gmail.com>
2023-05-30 18:17:14 +0300

    add profile data

--- src/container/profileForm/index.jsx
+++ src/container/profileForm/index.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { doc, setDoc, updateDoc , getDoc } from "firebase/firestore"; 
+import { db } from '../../firebaseConfig';
 import './index.css'
 
 
@@ -8,9 +10,16 @@ const ProfileForm = () => {
   const [faculty, setFaculty] = useState('');
   const [department, setDepartment] = useState('');
   const [grade, setGrade] = useState('');
-  const [email, setEmail] = useState('');
   const [universityEmail, setUniversityEmail] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [stNumber, setStNumber] = useState('');
+  const [authToken, setAuthToken] = useState('');
+  
+  useEffect(() => {
+    // Fetch data from API here
+    const fetchAuthToken = localStorage.getItem('authToken');
+    setAuthToken(fetchAuthToken);
+  }, []);
 
   const faculties = {
     'Faculty of Communication': ['Cartoon and Animation', 'Journalism'],
@@ -20,25 +29,61 @@ const ProfileForm = () => {
 
   const grades = ['first grade', 'second grade', 'third grade', 'fourth grade'];
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    // Process form data here
-  };
+  // const handleFormSubmit = (e) => {
+  //   e.preventDefault();
+  //   // Process form data here
+  // };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault(); // Prevent default form submission
+      let submittedData = {
+        stName: firstName,
+        stNum:  stNumber,
+        faculty: faculty,
+        department: department,
+        grade: grade,
+        phone: phoneNumber,
+        intern1: "open",
+        intern2: "open",
+      };
+      const profileRef = doc(db, 'users', authToken);
+      await setDoc(profileRef, submittedData, { merge: true });
+      console.log(submittedData)
+      // await updateDoc(doc(db, "internships", authToken), submittedData);
+      // if (internType === 'intern1') {
+      //   await updateDoc(doc(db, "users", authToken), {
+      //     intern1: status
+      //   });
+      //   setSubmitting(false);
+      // }else if (internType === 'intern2'){
+      //   await updateDoc(doc(db, "users", authToken), {
+      //     intern2: status
+      //   });
+      //   setSubmitting(false);
+      // }
+  }
+
+
 
   return (
     <div className="profile-form-outerContainer">
       <div className='profile-form-mainContainer'> 
-        <div className='profile-form-HeaderCont'>
-          <h1>Profile</h1>
-          <p>Update your personal details here.</p>
+        <div className='header-Main-coordinator-dashboeard'>
+          <div className='header-profile'>
+            <h1>Profile Information</h1>
+            <p>Update your personal details here.</p>
+          </div>
+          <div 
+            className='coordinatorSelectorSeparator'
+            style={{display:"flex", backgroundColor: "#C8D8D7",height: "5px"}}
+          >
+          </div>
         </div>
-        <div className='separator'>
-        </div>
-        <form onSubmit={handleFormSubmit}>
+        <form className='profil-form-cont' onSubmit={handleFormSubmit}>
           <div className="profile-form-first">
-            <div className="input-group">
+            <div className="input-group-profile">
               <label className='labelProfile' htmlFor="username">
-                Name:
+                Name Surname:
               </label>
               <input
                 className='inputProfile'
@@ -49,24 +94,25 @@ const ProfileForm = () => {
                 onChange={(e) => setFirstName(e.target.value)} 
               />
             </div>
-            <div className="input-group">
-              <label className='labelProfile' htmlFor="lastName">
-                Surname:
+            <div className="input-group-profile">
+              <label className='labelProfile' htmlFor="username">
+                Student Number:
               </label>
-              <input 
+              <input
                 className='inputProfile'
-                id='lastName'
+                id='stNumber' 
                 type="text" 
-                placeholder="Last Name" 
-                value={lastName} 
-                onChange={(e) => setLastName(e.target.value)} 
+                placeholder="First Name" 
+                value={stNumber} 
+                onChange={(e) => setStNumber(e.target.value)} 
               />
             </div>
-            <div className="input-group">
+            <div className="input-group-profile">
               <label className='labelProfile' htmlFor="faculty">
                 Faculty
               </label>
               <select 
+                className='inputProfile'
                 id='faculty'
                 value={faculty} 
                 onChange={(e) => setFaculty(e.target.value)}
@@ -79,7 +125,7 @@ const ProfileForm = () => {
                 ))}
               </select>
             </div>
-            <div className="input-group">
+            <div className="input-group-profile">
               <label className='labelProfile' htmlFor="department">
                 Department
               </label>
@@ -97,7 +143,7 @@ const ProfileForm = () => {
                   ))}
               </select>
             </div>
-            <div className="input-group">
+            <div className="input-group-profile">
               <label className='labelProfile' htmlFor="grade">
                 Grade
               </label>
@@ -114,34 +160,7 @@ const ProfileForm = () => {
                 ))}
               </select>
             </div>
-          </div>
-          <div className="profile-form-second">
-            <div className="input-group">
-              <label className='labelProfile' htmlFor="email">
-                Email Address
-              </label>
-              <input 
-                className='inputProfile'
-                htmlFor='email'
-                type="email" 
-                placeholder="Email Address" 
-                value={email} 
-                onChange={(e) => setEmail(e.target.value)} 
-              />
-            </div>
-            <div className="input-group">
-              <label className='labelProfile' htmlFor="universityEmail">
-                University Email
-              </label>
-              <input 
-                className='inputProfile'
-                type="email" 
-                placeholder="University Email" 
-                value={universityEmail} 
-                onChange={(e) => setUniversityEmail(e.target.value)} 
-              />
-            </div>
-            <div className="input-group">
+            <div className="input-group-profile">
               <label className='labelProfile' htmlFor="phoneNumber">
                 Phone Number
               </label>
@@ -155,7 +174,37 @@ const ProfileForm = () => {
                 />
             </div>
           </div>
-          <button className='buttonProfile' type="submit">Submit</button>
+          {/* <div className="profile-form-second"> */}
+            {/* <div className="input-group-profile">
+              <label className='labelProfile' htmlFor="universityEmail">
+                University Email
+              </label>
+              <input 
+                className='inputProfile'
+                type="email" 
+                placeholder="University Email" 
+                value={universityEmail} 
+                onChange={(e) => setUniversityEmail(e.target.value)} 
+              />
+            </div> */}
+            {/* <div className="input-group-profile">
+              <label className='labelProfile' htmlFor="phoneNumber">
+                Phone Number
+              </label>
+              <input 
+                className='inputProfile'
+                id='phoneNumber'
+                type="tel" 
+                placeholder="Phone Number" 
+                value={phoneNumber} 
+                onChange={(e) => setPhoneNumber(e.target.value)} 
+                />
+            </div>
+          </div> */}
+          <div className="profile-form-button">
+            <button
+              className='buttonProfile' type="submit">Submit</button>
+          </div>
         </form>
       </div>
     </div>
